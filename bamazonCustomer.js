aa5a7f86687963c5f926f35559b398641de36638
risaco <rhia.cooper@gmail.com>
2017-09-17 14:37:18 -0500

    update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-
+var inquirer = require("inquirer");
 var table = require("console.table");
 var mysql = require("mysql");
 
@@ -19,19 +19,68 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Print List of all itemes for sale
+
+// Print List of all itemes for sale, ask user which item and qty
 connection.query("SELECT * FROM bamazon.products", function(err, res){
 	if (err) throw err;
 	printList(res);
+
+  inquirer.prompt([
+    {
+      name: 'itemNumber',
+      message: "Which item would you like to buy? (ID)"
+    },{
+      name: 'quantity',
+      message: 'How many would you like to purchase?'
+    }
+    ]).then(function(answers) {
+      var itemId = parseFloat(answers.itemNumber);
+      var itemQty = parseFloat(answers.quantity);
+
+      checkStock(itemId, itemQty);
+  });
 });
 
 // ***** FUNCTIONS *****
+
+
 function printList(res){
 	console.log("\nItems for sale:\n");
-
+  console.log("\n---------------------------------------------\n");
 	for (i=0; i < res.length; i++) {
 		console.log("\nID: " + res[i].item_id + "\nProduct Name: " + res[i].product_name 
 			+ "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price);
 	}
 
+  console.log("\n---------------------------------------------\n");
+} // END of printList function
+
+
+function checkStock(itemNumber, quantity){
+
+  connection.query("SELECT * FROM bamazon.products WHERE ?", {
+    item_id: itemNumber
+  }, function(err, res){
+    if (err) throw err;
+
+    if (quantity <= res[0].stock_quantity) {
+
+      var orderTotal = quantity * res[0].price;
+
+      var newStock = res[0].stock_quantity - quantity;
+
+      connection.query("UPDATE bamazon.products SET ? WHERE ?",[
+        {
+          stock_quantity: newStock
+        }, {
+          item_id: itemNumber
+        }], function(err, res){
+          if (err) throw err;
+
+          console.log("\nYour order is complete. Total due: " + orderTotal);
+      });
+    } else {
+      console.log("Insufficient quantity!");
+    }
+  });
 }
--- bamazondb.sql
+++ bamazondb.sql
@@ -14,9 +14,9 @@ PRIMARY KEY (item_id)
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
-("Sony Alpha a6000 Mirrorless Digital Camera with 16-50mm Power Zoom Lens", "Electronics", 598.00, 15),
+("Sony Alpha a6000 Mirrorless Digital Camera with 16-50mm Power Zoom Lens", "Electronics", 598.01, 15),
 ("ThermoPro TP55 Digital Hygrometer Indoor Thermometer Humidity Gauge", "Electronics", 29.99, 5),
-("Born a Crime: Stories from a South African Childhood", "Books", 28.00, 23),
+("Born a Crime: Stories from a South African Childhood", "Books", 28.03, 23),
 ("I Can't Make This Up: Life Lessons", "Books", 26.99, 111),
 ("ELVIS Zebrawood Concert 23' Ukulele Acoustic for Professional Performance", "Musical Instruments", 109.12, 3),
 ("SUNYIN Wooden Kazoo, Exquisite Instrument", "Musical Instruments", 19.99, 13),
